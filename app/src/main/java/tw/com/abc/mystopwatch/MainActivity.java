ef623de58748081435aecddfd197c0830d0fa800
yadoch <yadoch@gmail.com>
2017-08-04 14:36:09 +0800

    版面初稿

--- app/src/main/java/tw/com/abc/mystopwatch/MainActivity.java
+++ app/src/main/java/tw/com/abc/mystopwatch/MainActivity.java
@@ -2,12 +2,38 @@ package tw.com.abc.mystopwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button Btn1,Btn2;
+    private boolean isRunning;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TextView tv=(TextView)findViewById(R.id.Btn1);
+        Btn1=(Button) findViewById(R.id.Btn1);
+        Btn2=(Button) findViewById(R.id.Btn2);
+
     }
+    public void doBtn1(View view){
+
+
+
+    }
+    // Start and Stop 按鍵觸發
+    public void doBtn2(View view){
+        isRunning = !isRunning;
+        if(isRunning){
+            Btn1.setText("Restart");
+            Btn2.setText("Stop!!");
+        }else {
+            Btn1.setText("Lap");
+            Btn2.setText("Start!!");
+        }
+    }
+
 }
